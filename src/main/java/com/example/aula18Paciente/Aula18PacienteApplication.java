ab7870f1edb3549f5789accd5a90e9c68c7afc83
katsuhisa91 <91katsuhisa@gmail.com>
2021-11-09 10:06:04 -0300

    aula18mesa

---
+++ src/main/java/com/example/aula18Paciente/Aula18PacienteApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula18Paciente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula18PacienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula18PacienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aula18Paciente/Aula18PacienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula18Paciente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula18PacienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
